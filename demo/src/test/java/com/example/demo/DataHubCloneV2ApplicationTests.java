c9b14ae6e73c388ed165b63babb9188b3e4a4bea
farellfebriano <farellfebriano8@gmail.com>
2024-06-13 11:30:05 -0500

    init_springboot

---
+++ demo/src/test/java/com/example/demo/DataHubCloneV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHubCloneV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
